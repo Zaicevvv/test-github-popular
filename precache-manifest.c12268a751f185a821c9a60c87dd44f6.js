1b48078250e276490d9904a9ddef3c1e8bbed7f7
Zaicev Vladimir <zaicevvv1991@gmail.com>
2019-11-17 10:31:15 +0200

    Updates

---
+++ precache-manifest.c12268a751f185a821c9a60c87dd44f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ebcff5fd4b982e5aac1db8251909fa",
+    "url": "/test-github-popular/index.html"
+  },
+  {
+    "revision": "568dccfd550090f925ba",
+    "url": "/test-github-popular/static/css/main.46a06091.chunk.css"
+  },
+  {
+    "revision": "eb34568e499f82e83cfb",
+    "url": "/test-github-popular/static/js/2.31fd9dbd.chunk.js"
+  },
+  {
+    "revision": "568dccfd550090f925ba",
+    "url": "/test-github-popular/static/js/main.675a25f7.chunk.js"
+  },
+  {
+    "revision": "e3fbccd814c1fdbc20f9",
+    "url": "/test-github-popular/static/js/runtime~main.a70ddeca.js"
+  }
+]);
